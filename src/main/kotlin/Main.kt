3c80f000a0ee8c097653e2927d8967d354d8f0ae
bartoszkruba <bartosz.kruba@gmail.com>
2019-12-31 12:18:27 +0100

    Moved all control mappings into single method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -62,9 +62,6 @@ private fun configureSocketEvents(socket: dynamic, gameEngine: GameEngine) {
     socket.on("playerName") { data ->
         ControlsMapper.processNameChange(data.name as String, socket.id as String, gameEngine)
     }
-    socket.on("mouseStart") { ControlsMapper.processMousePressed(socket.id as String, gameEngine) }
-    socket.on("mouseStop") { ControlsMapper.processMouseReleased(socket.id as String, gameEngine) }
     socket.on("playerRotation") { data -> ControlsMapper.processRotationChange(data, socket.id as String, gameEngine) }
-    socket.on("pickWeapon") { ControlsMapper.processPickWeaponPressed(socket.id as String, gameEngine) }
     socket.on("restart") { ControlsMapper.processRespawn(socket.id as String, gameEngine) }
 }
--- src/main/kotlin/server/ControlsMapper.kt
+++ src/main/kotlin/server/ControlsMapper.kt
@@ -6,30 +6,29 @@ import engine.Key
 class ControlsMapper {
     companion object {
         fun processKeyPressed(data: dynamic, agentId: String, engine: GameEngine) {
+
             when (data["key"]) {
                 KeyMappings.UP -> engine.setAgentKeyPressed(agentId, Key.UP)
                 KeyMappings.LEFT -> engine.setAgentKeyPressed(agentId, Key.LEFT)
                 KeyMappings.DOWN -> engine.setAgentKeyPressed(agentId, Key.DOWN)
                 KeyMappings.RIGHT -> engine.setAgentKeyPressed(agentId, Key.RIGHT)
                 KeyMappings.RELOAD -> engine.setAgentKeyPressed(agentId, Key.RELOAD)
+                KeyMappings.LEFT_MOUSE -> engine.setAgentMousePressed(agentId)
+                KeyMappings.PICK_WEAPON -> engine.setAgentPickWeapon(agentId, true)
             }
         }
 
         fun processKeyReleased(data: dynamic, agentId: String, engine: GameEngine) {
+
             when (data["key"]) {
                 KeyMappings.UP -> engine.setAgentKeyReleased(agentId, Key.UP)
                 KeyMappings.LEFT -> engine.setAgentKeyReleased(agentId, Key.LEFT)
                 KeyMappings.DOWN -> engine.setAgentKeyReleased(agentId, Key.DOWN)
                 KeyMappings.RIGHT -> engine.setAgentKeyReleased(agentId, Key.RIGHT)
                 KeyMappings.RELOAD -> engine.setAgentKeyReleased(agentId, Key.RELOAD)
+                KeyMappings.LEFT_MOUSE -> engine.setAgentMouseReleased(agentId)
             }
         }
-
-        fun processMousePressed(agentId: String, engine: GameEngine) =
-            engine.setAgentMousePressed(agentId)
-
-        fun processMouseReleased(agentId: String, engine: GameEngine) =
-            engine.setAgentMouseReleased(agentId)
 
         fun processNameChange(name: String, agentId: String, engine: GameEngine) {
             engine.setAgentName(agentId, name)
@@ -38,10 +37,6 @@ class ControlsMapper {
 
         fun processRotationChange(data: dynamic, agentId: String, engine: GameEngine) {
             engine.setAgentRotation(agentId, data.degrees as Float)
-        }
-
-        fun processPickWeaponPressed(agentId: String, engine: GameEngine) {
-            engine.setAgentPickWeapon(agentId, true)
         }
 
         fun processRespawn(agentId: String, engine: GameEngine) {
