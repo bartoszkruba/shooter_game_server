01043e1fa872c1360cf9f73241f7da64645ab1cd
bartoszkruba <bartosz.kruba@gmail.com>
2020-01-14 12:40:16 +0100

    settings values can be updated through html console

--- src/main/kotlin/settings/ui/Routes.kt
+++ src/main/kotlin/settings/ui/Routes.kt
@@ -63,8 +63,31 @@ fun configureRoutes(app: dynamic) {
     }
 
     app.post("/") { req, res, _ ->
+        WEAPON_RESPAWN_RATE = (req.body.weaponRespawnRate as String).toInt()
+        PISTOLS_ON_MAP = (req.body.pistolsOnMap as String).toInt()
+        MACHINE_GUNS_ON_MAP = (req.body.machineGunsOnMap as String).toInt()
+        SHOTGUNS_ON_MAP = (req.body.shotgunsOnMap as String).toInt()
+        BAZOOKAS_ON_MAP = (req.body.bazookasOnMap as String).toInt()
 
+        PISTOL_PROJECTILE_DAMAGE = (req.body.pistolDamage as String).toFloat()
+        MACHINE_GUN_PROJECTILE_DAMAGE = (req.body.machineGunDamage as String).toFloat()
+        SHOTGUN_PROJECTILE_DAMAGE = (req.body.shotgunDamage as String).toFloat()
+        BAZOOKA_EXPLOSION_DAMAGE = (req.body.bazookaExplosionDamage as String).toInt()
+        BARREL_EXPLOSION_DAMAGE = (req.body.barrelExplosionDamage as String).toInt()
 
+        ZOMBIE_RESPAWN_RATE = (req.body.zombieRespawnRate as String).toInt()
+        ZOMBIES_PER_RESPAWN = (req.body.zombiesPerRespawn as String).toInt()
+        MAX_ZOMBIES_ON_MAP = (req.body.zombiesOnMap as String).toInt()
+        ZOMBIE_ATTACK = (req.body.zombieAttackDamage as String).toInt()
+        ZOMBIE_ATTACK_SPEED = (req.body.zombieAttackSpeed as String).toInt()
+        ZOMBIE_MOVEMENT_SPEED = (req.body.zombieMovementSpeed as String).toFloat()
+
+        EXPLOSIVE_BARREL_RESPAWN_RATE = (req.body.barrelRespawnRate as String).toInt()
+        EXPLOSIVE_BARREL_RESPAWN_RATE = (req.body.barrelsPerRespawn as String).toInt()
+
+        PLAYER_BASE_HEALTH = (req.body.playerHealth as String).toInt()
+        ZOMBIE_BASE_HEALTH = (req.body.zombieHealth as String).toInt()
+        INVINCIBILITY_DURATION = (req.body.invincibilityDuration as String).toInt()
 
         res.redirect("/")
     }
